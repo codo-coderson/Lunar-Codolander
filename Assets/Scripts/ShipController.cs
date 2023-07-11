a380888f3a499820fa8c585f27db535f55950a4d
Codo <codocoderson@gmail.com>
2023-07-11 23:33:56 +0200

    landing mechanics finished

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 public class ShipController : MonoBehaviour
 {
     private Rigidbody2D ship;
-    //private PolygonCollider2D bodyCollider;
-    //private BoxCollider2D leftLandingGearCollider;
-    //private CapsuleCollider2D rightLandingGearCollider;
+    private int frameCount;
     private EdgeCollider2D landingGearCollider;
     public float rotationSpeed;
     public ParticleSystem thruster;
@@ -17,16 +15,22 @@ public class ShipController : MonoBehaviour
     AudioSource audioCrash;
     AudioSource audioThrust;
     AudioSource audioLanded;
+    private bool rotationOK;
+    private bool xVelocityOK;
+    private bool yVelocityOK;
+    private bool isCollision = false;
 
 
     private void Start()
     {
+        frameCount = 0;
         ship = GetComponent<Rigidbody2D>();
         landingGearCollider = GetComponent<EdgeCollider2D>();
         thruster = GetComponentInChildren<ParticleSystem>();
         var emission = thruster.emission;
         emission.enabled = false;
         applyThrust = false;
+        isCollision = false;
         rotationSpeed = 200f;
 
         audioCrash = gameObject.AddComponent<AudioSource>();
@@ -35,11 +39,20 @@ public class ShipController : MonoBehaviour
         audioThrust.clip = thrust;
         audioLanded = gameObject.AddComponent<AudioSource>();
         audioLanded.clip = landed;
+
+        // Let's set an initial x speed, currently for debugging purposes
+        Vector2 initialVelocity = new Vector2(0f, 0f);
+        ship.velocity = initialVelocity;
+        // This doesn't work --> ship.velocity.Set(0.06f, 0f);
+
+
+        // Let's check the speed in the Console, every second
+        //InvokeRepeating("LogEverySecond", 0f, 1f);
     }
 
     private void Update()
     {
-        Debug.Log("Vertical Speed: " + Mathf.Abs(ship.velocity.y) + "Horizontal Speed: " + Mathf.Abs(ship.velocity.x));
+
 
         if (Input.GetKeyDown(KeyCode.W))
         {
@@ -67,10 +80,25 @@ public class ShipController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Increment the frame count
+        frameCount++;
+
         if (applyThrust)
         {
             ship.AddForce(transform.up * 0.3f, ForceMode2D.Force);
         }
+
+        //Check if a number of frames have passed
+        if (isCollision)
+        {
+            Debug.Log("---------------------------------------------------------------------------------------------------");
+            Debug.Log("Framecount: " + frameCount);
+            Debug.Log("Vertical Speed: " + Mathf.Abs(ship.velocity.y) + ", Horizontal Speed: " + Mathf.Abs(ship.velocity.x));
+        }
+
+        rotationOK = (Mathf.Abs(ship.transform.rotation.z) < 0.02);
+        xVelocityOK = Mathf.Abs(ship.velocity.x) < 0.0008f;
+        yVelocityOK = Mathf.Abs(ship.velocity.y) < 0.04f;
     }
 
     private void RotateShip(float rotationInput)
@@ -81,29 +109,54 @@ public class ShipController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // If the ship lands with the landing gear,
+        // and it's straight 
+        // and it's moving horizontally and vertically slow
+        // then the landing is right
 
-        // If the ship lands with the landing gear
+        isCollision = true;
+
         if (collision.otherCollider.GetType() == typeof(BoxCollider2D))
         {
-            // If it's straight
-            if (Mathf.Abs(ship.transform.rotation.z) < 0.02)
+            if (rotationOK)
             {
-                // if the speed is ok
-                if (Mathf.Abs(ship.velocity.x) < 0.015f && Mathf.Abs(ship.velocity.y) < 0.05f)
+                if (xVelocityOK)
                 {
-                    shipLands();
+                    if (yVelocityOK)
+                    {
+                        shipLands();                        
+                    }
+                    else
+                    {
+                        shipCrashes();
+                        Debug.Log("Reason of crash: vertical speed");
+                    }
                 }
-                else shipCrashes();
+                else
+                {
+                    shipCrashes();
+                    Debug.Log("Reason of crash: horizontal speed");
+                }
             }
-            else shipCrashes();
+            else
+            {
+                shipCrashes();
+                Debug.Log("Reason of crash: not straight");
+            }
         }
-        else shipCrashes();
+        else
+        {
+            shipCrashes();
+            Debug.Log("Reason of crash: not landed with landing gear");
+        }
+
+        isCollision = false;
     }
 
     private void shipCrashes()
     {
         if (!audioCrash.isPlaying)
-        audioCrash.Play();
+            audioCrash.Play();
     }
 
     private void shipLands()
@@ -111,4 +164,10 @@ public class ShipController : MonoBehaviour
         if (!audioLanded.isPlaying)
             audioLanded.Play();
     }
+
+
+/*    void LogEverySecond()
+    {
+        Debug.Log("Frame count: " + frameCount + "Vertical Speed: " + Mathf.Abs(ship.velocity.y) + ", Horizontal Speed: " + Mathf.Abs(ship.velocity.x));
+    }*/
 }
